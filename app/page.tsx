97e232189cd147a204511c627924a4e75ff9172a
Koh Xuan Qi <kohhxuanqi@gmail.com>
2024-10-23 16:28:48 +0800

    minor changes to footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,20 @@
-// app/page.tsx
-
 import TextReader from "./components/TextReader";
 
 const Home = () => {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen bg-background">
-      <h1 className="text-4xl font-extrabold text-primary mb-6">Speed Reader</h1>
+      <h1 className="text-4xl font-extrabold text-primary mb-6">SpeedReader</h1>
       <TextReader />
       <footer className="mt-10 text-secondary text-sm">
-        &copy; {new Date().getFullYear()} Speed Reader App. All rights reserved.
+        &copy; {new Date().getFullYear()}{" "}
+        <a 
+          href="https://kohxuanqi.com"
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="hover:underline"
+        >
+          Koh Xuan Qi
+        </a>. All rights reserved.
       </footer>
     </main>
   );
